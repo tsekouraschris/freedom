ef5086e292655dd9cc5294d8531ce1087797921c
Chris Tsekouras <tsekouraschris0@gmail.com>
2020-09-11 15:06:01 +0300

    test1

--- engine.php
+++ engine.php
@@ -3,16 +3,27 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$dbname = "test";
+$message = $_POST['message']
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
+$sql = "INSERT INTO messages (content)
+VALUES ($message)";
+
+if ($conn->query($sql) === TRUE )
+{
+echo "new message uploaded";
+}else
+{
+echo "Error: ".$sql."<br>".$conn->error;
+}
 
 
 ?>
