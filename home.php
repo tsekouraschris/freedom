01614e6ee559608d852936e646b8ca1265ea943e
Chris Tsekouras <tsekouraschris0@gmail.com>
2020-09-11 14:54:15 +0300

    engnine

---
+++ home.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+
+<body>
+    <h1>upload any message</h1>
+    <form action="engine.php" method="POST">
+    <textarea name="message"></textarea>
+    </form>
+</body>
+
+</html>
