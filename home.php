04ae1109729b07b074a3f62bffba11aac7c1c7f0
Chris Tsekouras <tsekouraschris0@gmail.com>
2020-09-11 14:57:01 +0300

    nothing special

--- home.php
+++ home.php
@@ -5,6 +5,8 @@
     <h1>upload any message</h1>
     <form action="engine.php" method="POST">
     <textarea name="message"></textarea>
+    <input type="submit"></input>
+    <p>dont worry, reviewers will check your post before it reaches the wrong audience</p>
     </form>
 </body>
 
